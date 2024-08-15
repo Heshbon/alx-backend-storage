9cf01349bec33a54e22edb5496d5d9e4c9872bb7
Heshbon <cityalight.hesbon@gmail.com>
2024-08-15 00:11:09 +0000

    web.py

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+""" A get_page function to fetch HTML content of URLs with caching
+"""
+
+import redis
+import requests
+from functools import wraps
+
+r = redis.Redis()
+
+
+def url_access_count(method):
+    """Decorator to enhance get_page with caching."""
+    @wraps(method)
+    def wrapper(url):
+        """Cache results and count URL access."""
+        # Increment the count
+        key_count = "count:" + url
+        current_count = r.incr(key_count)
+
+        # Check if the URL is cached
+        key = "cached:" + url
+        cached_value = r.get(key)
+        if cached_value:
+            # If cached, return cached value
+            return cached_value.decode("utf-8")
+        else:
+            # If not cached, fetch the content and cache it
+            html_content = method(url)
+            r.set(key, html_content, ex=10)
+            return html_content
+        return wrapper
+
+
+@url_access_count
+def get_page(url: str) -> str:
+    """Fetch and return the HTML content"""
+    results = requests.get(url)
+    return results.text
+
+
+if __name__ == "__main__":
+    get_page('http://slowwly.robertomurray.co.uk')
